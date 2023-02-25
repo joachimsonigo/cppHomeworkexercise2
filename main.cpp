91f1d789fd591edf3a70d22595b2b10b378ede18
Joachim Sonigo <asdfno123123@gmail.com>
2023-02-25 13:22:22 +0100

    2nd Version with some testing and a lot of operators plus all the base methods

--- main.cpp
+++ main.cpp
@@ -25,10 +25,16 @@ public:
     //Overloading Operators
     complex operator +(const complex&);
     complex operator -(const complex&);
+    complex operator *(const complex&);
+    complex operator /(const complex&);
     void operator +=(const complex&);
     void operator -=(const complex&);
     bool operator ==(const complex&);
     bool operator !=(const complex&);
+
+
+    friend ostream& operator <<(ostream&,const complex&);
+    friend istream& operator >>(istream&,complex&);
 };
 
 //Defining the methods outside of the class structure
@@ -36,6 +42,7 @@ complex::complex() {x=0;y=0;}
 complex::complex(double a) {x=a;y=a;}
 complex::complex(double a, double b) {x=a;y=b;}
 complex::complex(const complex &c) {x=c.x;y=c.y;}
+complex::~complex() {}
 void complex::set_x(double a) {x=a;}
 void complex::set_y(double b) {y=b;}
 double complex::get_x() {return x;}
@@ -43,18 +50,22 @@ double complex::get_y() {return y;}
 
 complex complex::operator +(const complex &c) {complex R;R.x=x+c.x;R.y=y+c.y;return R;}
 complex complex::operator -(const complex &c) {complex R;R.x=x-c.x;R.y=y-c.y;return R;}
+complex complex::operator*(const complex &c) {complex R;R.x=x*c.x;R.y=y*c.y;return R;}
 void complex::operator +=(const complex &c) {x=c.x+c.x;y=c.y+c.y;}
 void complex::operator -=(const complex &c) {x=c.x-c.x;y=c.y-c.y;}
 bool complex::operator ==(const complex &c) {if(x==c.x && y==c.y)return 1;else{return 0;}}
 bool complex::operator !=(const complex &c) {if(x != c.x || y != c.y)return 1;else{return 0;}}
 
-
+ostream& operator <<(ostream&os,const complex&p){os<<"x= "<<p.x<<" et y= "<<p.y<<endl;return(os);};
+istream& operator >>(istream&is, complex&p){cout<<"Value of x:";is>>p.x;cout<<"Value of y :";is>>p.y;};
 
 
 //-------------------
 //      Main
 //-------------------
 int main() {
-    cout << "Hello, World!" << endl;
+    complex c1(2),c2(10,1),c3;
+    c3 = c1*c2;
+    cout<<c3<<endl;
     return 0;
 }
